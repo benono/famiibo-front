3f3cc85a752f0d84e91bd055ef1730d3d4209f40
BenOno <bentoki.1213@gmail.com>
2024-07-17 07:53:54 -0700

    Add user in context

--- context/auth.tsx
+++ context/auth.tsx
@@ -1,9 +1,12 @@
 import { createContext, useState, useEffect, useContext } from 'react'
 import apiClient from '../app/lib/apiClient'
+import { useRouter } from 'next/navigation'
 
 interface AuthContextType {
+  user: { id: string; username: string; email: string } | null
   login: (token: string) => void
   logout: () => void
+  loading: boolean
 }
 
 interface AuthProviderProps {
@@ -11,8 +14,10 @@ interface AuthProviderProps {
 }
 
 const AuthContext = createContext<AuthContextType>({
+  user: null,
   login: () => {},
   logout: () => {},
+  loading: true,
 })
 
 export const useAuth = () => {
@@ -20,23 +25,62 @@ export const useAuth = () => {
 }
 
 export const AuthProvider = ({ children }: AuthProviderProps) => {
-  const [user, setUser] = useState(null)
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [user, setUser] = useState<{
+    id: string
+    username: string
+    email: string
+  } | null>(null)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const token = localStorage.getItem('auth_token')
-    apiClient.defaults.headers['Authorization'] = `Bearer ${token}`
+    if (token) {
+      apiClient.defaults.headers['Authorization'] = `Bearer ${token}`
+      apiClient
+        .get('/users/me/')
+        .then((res) => {
+          console.log('user', res.data.user)
+          setUser(res.data.user)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+        .finally(() => {
+          setLoading(false)
+        })
+    } else {
+      setLoading(false)
+    }
   }, [])
+
+  useEffect(() => {
+    console.log('user', user)
+  }, [user])
 
   const login = async (token: string) => {
     localStorage.setItem('auth_token', token)
+    apiClient.defaults.headers['Authorization'] = `Bearer ${token}`
+    apiClient
+      .get('/users/me/')
+      .then((res) => {
+        console.log('user', res.data.user)
+        setUser(res.data.user)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
   }
 
   const logout = () => {
+    apiClient.defaults.headers['Authorization'] = ''
     localStorage.removeItem('auth_token')
+    setUser(null)
   }
 
-  const value = { login, logout }
+  const value = { login, logout, user, loading }
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
 }
